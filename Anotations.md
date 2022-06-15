99afa2448ec9fdd023e7fa57f67cfdac613a9cfa
ZezinZK <josevogeleysa2016@gmail.com>
2022-06-15 09:52:58 -0300

    Added file Anotations.md and Anchor expressions in teste_html_php.php

---
+++ Anotations.md
@@ -0,0 +1,11 @@
+# Anotações HTML
+    <p>Serve para paragrafos</p> ->aceitando imagens e outros conteudos
+    
+    <a>LINK</a> ->Ancora
+    
+    <a href="linkedin.com/...">Linkedin link</a> ->exemplo pra sites
+    
+    <a href="mailto:exemplo123@exemplo.com">Email</a> ->No caso de emails e telefone, deve-se utilizar o 'mailto' e 'tel' 
+    respectivamente*
+    
+    <a target="_blank">Link</a> ->Target diz como o link vai ser aberto, nesse caso _blank seria uma nova aba no browser.
--- teste_HTML_PHP.php
+++ teste_HTML_PHP.php
@@ -26,7 +26,16 @@
                 <header>
                     <h3>Post #1</h3>
                 </header>
+                <p>
+                    Este Texto aqui serve apenas como meio de 
+                    exemplificar a utilização do parágrafo no html 
+                    e a utilização de links, então segue o link do meu github e do meu email
+                    para abrir numa nova aba utilizando o target=_blank 
+                    <a href="https://github.com/ZezinZK/PHP-HTML-INIT" target="_blank">Link do git</a> 
+                    <a href="mailto:josevogeleysa2016@gmail.com">email</a> e telefone caso esteja vendo pelo celular 
+                    <a href="tel:+5581918182828">telefone</a>
 
+                </p>
             </article>
         </section>
 
